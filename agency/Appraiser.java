f33601f1629ede5d64a891c5fd1c5337f9d11a7d
ErskineR7 <43677279+ErskineR7@users.noreply.github.com>
2018-11-15 14:06:39 -0500

    Update Appraiser.java

--- agency/Appraiser.java
+++ agency/Appraiser.java
@@ -1,111 +1,96 @@
-package agency;
+package Acme_Realty;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
-   /** The appraiser compares the property with three 
-	 * or four similar homes that have sold in the area,
-	 * often called comparables, or comps. 
+   /** The Appraiser class will assign a value to a home based on it's internal analyzer.
 	 * 
 	 *  The analysis considers specific components, such as: 
-		 * -lot size, 
-		 * -square footage of finished and unfinished space, 
-		 * -style 
-		 * -age of house 
-		 * -statements regarding serious structural problems, 
-		 *  such as wet basements and cracked foundations
-		 * -notes about the surrounding area, such as new or 
-		 *  established development, rural acreage, and so on.
-		 * -other features such as garages and fireplaces.
+		 * -square footage 
+		 * -acreage 
+		 * -other features such as security systems, pools and fireplaces.
 	 *
-	 * A common misunderstanding is that the appraisal amount is 
-	 * only for the house itself. In fact, the figure appraises the 
-	 * total value of the home and any other permanent structures, 
-	 * along with the land that the house is built on. 
+	 * - edited by R.Erskine 11/15/18
 	 */
 public class Appraiser { 
 	
-	private Agency propertySpecs; // Specifications of the seller's property 
-	private double averagePrice; // Average price of similar properties
-	private ArrayList<HousingList> toBeSold; // List of houses for sale
-	private ArrayList<ArchivedProperties> listing; // List of sold properties
-	private ArrayList<ArchivedProperties> comparables; // List of similar sold properties
+	//the built in prices the Appraiser uses to calculate the cost of a house
+	private double pricePerSQFT;
+	private double pricePerAcre;
+	private double priceSecSys;
+	private double pricePool;
+	private double priceFire;
+	
+	//the house costs
+	private double propertyValue;
 	
 	/** 
-	 * constructor to receive data only from the Agency
+	 * default constructor
+	 * 		This is the ACME appraiser, with no custom pricing
 	 */
-	public Appraiser(Agency propertySpecs) 
+	public Appraiser() 
 	{
-		this.propertySpecs = propertySpecs;
-		averagePrice = 0;
-		toBeSold = new ArrayList<>();
-		comparable = new ArrayList<>();
+
+		pricePerSQFT = 150.00;
+		pricePerAcre = 10000;
+		priceSecSys = 15000;
+		pricePool = 20000;
+		priceFire = 7000;
+		
+		propertyValue = 0.0;
 	}
 
-	/** 
-	 * Matches seller's info with sold properties from ArchivedProperties
-	 */
-	public void compareProperties() 
-	{
-		for(ArchivedProperties a: listing) {
-			/**'if' statements to sort similar properties
-			 *  save in similar properties 
-			 *  in collection comparables
-			 *
-			 *
-			 */ 
-				comparables.add(a);
-			
-			/**else 
-	    	 *	display: No properties found or exception could be added 	
-	    	 */
-		}
-	}
 
 	/** 
 	 * Calculate price of seller's property
 	 */ 
-	public void calculatePropertyPrice() 
+	public void calculatePropertyValue(int sqft, double acre, boolean sec, boolean pool, boolean fire) 
 	{	
-		double total = 0;
-		for(ArchivedProperties a : comparables) 
+		double total = 0.0;
+		
+		total += sqft * pricePerSQFT;
+		total += acre * pricePerAcre;
+		
+		if(sec == true)
 		{
-			total += a.getPrice(); // method must be in ArchivedProperties
+			total += priceSecSys;
 		}
 		
-		averagePrice = total / comparables.size();
+		if(pool == true)
+		{
+			total += pricePool;
+		}
 		
-		/** add or reduce price according to state of the property
-		 *  
-		 *  We could have a separate class that handles the deduction
-		 *  or addition of property value:
-		 *  		 e.g. +$2,000 for newly installed bathtub
-		 *                -$500 for broken doors  
-		 */
-		averagePrice += // renovation, land, etc.
-		averagePrice -= // structural problems, house age, etc.			
+		if(fire == true)
+		{
+			total += priceFire;
+		}
+	
+		propertyValue = total;
+	
 	}
 
 	/** 
 	 * Return calculated price of property 
 	 */ 
-	public double getPropertyPrice() 
+	public double getPropertyValue() 
 	{
-		return averagePrice;
+		return propertyValue;
 	}
-	
-	/** 
-	 * Set obtained average price to new property
-	 */  
-	public void setPropertyPrice(double averagePrice) 
-	{
-		this.averagePrice = averagePrice;
-	}
-	
+		
 	/** 
 	 * Add property to Housing List Database
 	 */  
-	public void addPropertyToDatabase(Agency propertyAppraised) 
-	{
-		toBeSold.add(propertyAppraised);
+	public void addPropertyToDatabase(Property prop) 
+			throws IOException {
+		
+		    BufferedWriter writer = new BufferedWriter(new FileWriter("PropertyList.txt"));
+		    writer.write(prop.propWrite());
+		     
+		    writer.close();
+
 	}
 }
+
