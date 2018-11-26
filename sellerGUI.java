35a5683abde769786915bf8cd3c907f12e6a1c62
ErskineR7 <43677279+ErskineR7@users.noreply.github.com>
2018-11-25 22:45:36 -0500

    Update sellerGUI.java
    This all works now!

--- sellerGUI.java
+++ sellerGUI.java
@@ -1,203 +1,163 @@
-package agency;
 
 import java.awt.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import javax.swing.*;
+import java.text.DecimalFormat;
 
 public class sellerGUI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
-	//Property toSell;
-	//Appraiser acme;
-	JOptionPane listConfirm;
-	JLabel typeLabel, sqftLabel, roomLabel, poolLabel, fireplaceLabel, garageLabel, securityLabel, bathLabel, yardLabel,
-			builtLabel, addressLabel;
 	JComboBox<String> typeSelect;
-	JTextField sqftInput, roomInput, addressInput, bathInput, yardInput, builtInput;
-	JRadioButton poolButton, fireplaceButton, garageButton, securityButton;
+	
+	JPanel set1, set2, set3;
+	
 	boolean hasPool, hasSecurity, hasFireplace, hasGarage;
 	int rooms, sqft, builtIn;
 	double bath, yard;
 	String address, type;
 	JButton submit;
-
-	JPanel set1;
-	JPanel set2;
-	JPanel set3;
+	Property toSell;
 
 	public sellerGUI() {
 		super("sellerGUI");
+		
+		hasPool = hasSecurity = hasFireplace = hasGarage = false;
+		
+		int r = 0;
+		
+		String[] possibleValues = { "Colonial", "Ranch", "Bungalow", "Victorian", "BiLevel", "Mobile",
+									"Commercial", };
+		
+		
+		address = JOptionPane.showInputDialog("What is the Address?");
+		type = (String) JOptionPane.showInputDialog(null,
+				"What type of property is it?", "Choose One",
+				JOptionPane.INFORMATION_MESSAGE, null,
+				possibleValues, possibleValues[0]);
+		builtIn = Integer.parseInt(JOptionPane.showInputDialog("When was the property built?"));
+		sqft = Integer.parseInt(JOptionPane.showInputDialog("How many square feet is the property?"));
+		rooms = Integer.parseInt(JOptionPane.showInputDialog("How many bedrooms are in the property?"));
+		bath = Double.parseDouble(JOptionPane.showInputDialog("How many bathrooms are in the property?"));
+		yard = Double.parseDouble(JOptionPane.showInputDialog("How much acreage does the property have?"));
+		
+		r = JOptionPane.showConfirmDialog(null,
+				"Does the property have a pool?", "Choose One", JOptionPane.YES_NO_OPTION);
+		if (r == 0)
+		{
+			hasPool = true;
+		}
+		
+		r = JOptionPane.showConfirmDialog(null,
+				"Does the property have a security system?", "Choose One" , JOptionPane.YES_NO_OPTION);
+		if (r == 0)
+		{
+			hasSecurity = true;
+		}
+		
+		r = JOptionPane.showConfirmDialog(null,
+				"Does the property have a fireplace?", "Choose One" , JOptionPane.YES_NO_OPTION);
+		if (r == 0)
+		{
+			hasFireplace = true;
+		}
+		
+		r = JOptionPane.showConfirmDialog(null,
+				"Does the property have a garage?", "Choose One" , JOptionPane.YES_NO_OPTION);
+		if (r == 0)
+		{
+			hasGarage = true;
+		}
+		
 		makeSellerGUI();
 	}
 
 	public void makeSellerGUI() {
 		
-		this.setTitle("List Property for Sale");
 		this.setSize(400, 400);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLayout(new BorderLayout());
 
-		listConfirm = new JOptionPane();
-
-		addressLabel = new JLabel("Address: ");
-		typeLabel = new JLabel("Property type: ");
-		builtLabel = new JLabel("Built In: ");
-		sqftLabel = new JLabel("Number of Square Footage: ");
-		roomLabel = new JLabel("Number of Rooms: ");
-		bathLabel = new JLabel("Number of Bathrooms: ");
-		yardLabel = new JLabel("Size of Yard (in acres): ");
-		securityLabel = new JLabel("Security System: ");
-		fireplaceLabel = new JLabel("Fireplace: ");
-		garageLabel = new JLabel("Garage: ");
-		poolLabel = new JLabel("Pool: ");
-		addressInput = new JTextField();
-		typeSelect = new JComboBox<>();
-		builtInput = new JTextField();
-		sqftInput = new JTextField();
-		roomInput = new JTextField();
-		bathInput = new JTextField();
-		yardInput = new JTextField();
-		poolButton = new JRadioButton();
-		garageButton = new JRadioButton();
-		fireplaceButton = new JRadioButton();
-		securityButton = new JRadioButton();
-		hasPool = hasSecurity = hasGarage = hasFireplace = false;
-		rooms = sqft = builtIn = 0;
-		bath = yard = 0.0;
-
-		submit = new JButton("Submit");
-		submit.addActionListener(e -> makeProperty());
-
+		setTitle("Review your Input");
+		setLayout(new BorderLayout());
 		
 		set1();
 		set2();
 		set3();
-
-		this.getContentPane().add(set1, BorderLayout.WEST);
-		this.getContentPane().add(set2, BorderLayout.CENTER);
-		this.getContentPane().add(set3, BorderLayout.EAST);
-		this.getContentPane().add(submit, BorderLayout.SOUTH);
-
-		typeSelect.addItem("Apartment");
-		typeSelect.addItem("Bungalow");
-		typeSelect.addItem("Business");
-		typeSelect.addItem("Cape Cod");
-		typeSelect.addItem("Colonial");
-		typeSelect.addItem("Commercial");
-		typeSelect.addItem("Condo");
-		typeSelect.addItem("Ranch");
-		typeSelect.addItem("TownHouse");
-		typeSelect.addItem("Victorian");
-
+		
+		add(set1,BorderLayout.WEST);
+		add(set2, BorderLayout.CENTER);
+		add(set3, BorderLayout.EAST);
+		
+		submit = new JButton("Accept Input");
+		submit.addActionListener(e -> makeProperty());
+		add(submit, BorderLayout.SOUTH);
+		
 		pack();
 		setVisible(true);
 	}
-
-	private void set1() {
+	
+	private void set1()
+	{
 		set1 = new JPanel();
-		set1.setLayout(new GridLayout(4, 2));
-
-		set1.add(addressLabel);
-		set1.add(addressInput);
-		set1.add(typeLabel);
-		set1.add(typeSelect);
-		set1.add(builtLabel);
-		set1.add(builtInput);
-		set1.add(sqftLabel);
-		set1.add(sqftInput);
+		set1.setLayout(new GridLayout(4,2));
+		
+		JLabel aLabel = new JLabel("Address: " + address);
+		set1.add(aLabel);
+		JLabel tLabel = new JLabel("Property type: " + type);
+		set1.add(tLabel);
+		JLabel btLabel = new JLabel("Built In: " + builtIn);
+		set1.add(btLabel);
+		JLabel sLabel = new JLabel("Number of Square Footage: " + sqft);
+		set1.add(sLabel);
+		
 	}
-
-	private void set2() {
+	
+	private void set2()
+	{
 		set2 = new JPanel();
-		set2.setLayout(new GridLayout(4, 2));
-
-		set2.add(roomLabel);
-		set2.add(roomInput);
-
-		set2.add(bathLabel);
-		set2.add(bathInput);
-
-		set2.add(yardLabel);
-		set2.add(yardInput);
+		set2.setLayout(new GridLayout(4,2));
+		
+		JLabel rLabel = new JLabel("Number of Rooms: " + rooms);
+		set2.add(rLabel);
+		JLabel baLabel = new JLabel("Number of Bathrooms: " + bath);
+		set2.add(baLabel);
+		JLabel yLabel = new JLabel("Size of Yard (in acres): " + yard);
+		set2.add(yLabel);
+		
 	}
-
-	private void set3() {
+	
+	private void set3()
+	{
 		set3 = new JPanel();
-		set3.setLayout(new GridLayout(4, 2));
-
-		set3.add(poolLabel);
-		set3.add(poolButton);
-
-		set3.add(garageLabel);
-		set3.add(garageButton);
-
-		set3.add(fireplaceLabel);
-		set3.add(fireplaceButton);
-
-		set3.add(securityLabel);
-		set3.add(securityButton);
+		set3.setLayout(new GridLayout(4,2));
+		
+		JLabel secLabel = new JLabel("Security System: " + hasSecurity);
+		set3.add(secLabel);
+		JLabel firLabel = new JLabel("Fireplace: " + hasFireplace);
+		set3.add(firLabel);
+		JLabel garLabel = new JLabel("Garage: " + hasGarage);
+		set3.add(garLabel);
+		JLabel poLabel = new JLabel("Pool: " + hasPool);
+		set3.add(poLabel);
+		
 	}
-
-	//private class makeProperty implements ActionListener {
-
-		//public void actionPerformed(ActionEvent e) {
+	
+	
 public void makeProperty()
 {
-	
 			
-
-			address = addressInput.getText();
-			type = typeSelect.getToolTipText();
 			
-			//The main issue was happening in this block
-			
-//			builtIn = Integer.parseInt(builtInput.getText());
-//			rooms = Integer.parseInt(roomInput.getText());
-//			sqft = Integer.parseInt(sqftInput.getText());
-//			bath = Double.parseDouble(bathInput.getText());
-//			yard = Double.parseDouble(yardInput.getText());
-			
-			// the input needed to be validated before parsing the string 
-			// to avoid getting a numberFormatException
-			
-			String built = builtInput.getText();
-	        if(built.matches("[0-9]+"))          
-	            builtIn = Integer.parseInt(built);
-			
-	        String room = roomInput.getText();
-	        if(room.matches("[0-9]+"))
-	        	rooms = Integer.parseInt(room);
-	       
-	        String foot = sqftInput.getText();
-	        if(foot.matches("[0-9]+"))
-	        	sqft = Integer.parseInt(foot);
-	        
-	        String bathroom = bathInput.getText();
-	        if(bathroom.matches("[0-9]+"))
-	        	 bath = Double.parseDouble(foot);
-			
-	        String backyard = yardInput.getText();
-	        if(backyard.matches("[0-9]+"))
-	        	 yard = Double.parseDouble(foot);
-	   
-			
-			if (securityButton.isSelected()) {
-				hasSecurity = true;
+			if (type == "Commercial")
+				{
+						toSell = new Commercial();
+				}
+			else
+			{
+				toSell = new Residential();
 			}
-			if (fireplaceButton.isSelected()) {
-				hasFireplace = true;
-			}
-			if (garageButton.isSelected()) {
-				hasGarage = true;
-			}
-			if (poolButton.isSelected()) {
-				hasPool = true;
-			}
-			
-			Property toSell = new Property();
 
 			toSell.setAddress(address);
 			//I used a println to see if the values where being passed to the fields
@@ -231,8 +191,12 @@ public void makeProperty()
 				e1.printStackTrace();
 			}
 			
+			DecimalFormat dollar = new DecimalFormat("0.00");
+			
 			JOptionPane.showMessageDialog(null,
-					"Congrats! You just listed a property for sale! We value the property at " + toSell.getValue());
+					"Congrats! You just listed a property for sale! We value the property at " + dollar.format(toSell.getValue()));
+			
+			System.exit(0);
 		
 	}}
 
