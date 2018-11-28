c7104ccaeefa3d9c56747500658abffaad74fb2b
denisha32 <43677295+denisha32@users.noreply.github.com>
2018-11-28 13:26:00 -0500

    Update FileReader.java

--- FileReader.java
+++ FileReader.java
@@ -1,83 +1,126 @@
-package agency;
+package Agency;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
- * A class to read CSV-style records of animal sighting reports.
- * 
- * @author David J. Barnes and Michael Kölling
- * @version 2016.02.29
+ * A class to read Property records
  */
-public class FileReader {
-	// How many fields are expected.
-	private static final int PROPERTIES = 11;
-	// Index values for the fields in each record.
-	private static final int FIELDS = 0, ADDRESS = 1, PROPERTY_TYPE = 2, BUILT_IN = 3, SQUARE_FOOT = 4, NUM_ROOMS = 5,
-			NUM_BATH = 6, NUM_FLOORS = 7, BACKYARD = 8, RENT_ZONING = 9, HAS_SEC_SYSTEM = 10, HAS_POOL = 11,
-			HAS_FIREPLACE = 12, HAS_GARAGE = 13;
+public class FileHandler {
+
+	ArrayList<Property> propList;
+
+	String address;
+	String propertyType;
+	int built;
+	int squareFoot;
+	int numRooms;
+	double numBath;
+	double backyard;
+	boolean rentZoning;
+	boolean has_secSystem, has_pool, has_fireplace, has_garage = false;
 
 	/**
-	 * Create a SightingReader.
+	 * Write info into specified file
 	 */
-	public FileReader() {
+	public void writeFile(Property prop, File filename) throws IOException {
+
+		BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true));
+		writer.write(prop.propWrite() + System.getProperty("line.separator"));
+
+		writer.close();
 	}
 
 	/**
-	 * Read sightings in CSV format from the given file. Return an ArrayList of
-	 * Sighting objects created from the information in the file.
-	 * 
-	 * @param filename The file to be read - should be in CSV format.
-	 * @return A list of Sightings.
+	 * Reads data from specified file and turns it into a Property object
 	 */
-	public ArrayList<Property> getPropertiesToBeSold(String filename) {
-		// Create a Sighting from a CSV input line.
-		Function<String, Property> createPropertyList = record -> {
-			String[] parts = record.split(",");
-			if (parts.length == FIELDS) {
-				try {
+	public void retrievePropertyObject(File filename) throws IOException {
 
-					String address = parts[ADDRESS].trim();
-					String propertyType = parts[PROPERTY_TYPE].trim();
-					int builtIn = Integer.parseInt(parts[BUILT_IN].trim());
-					int squareFoot = Integer.parseInt(parts[SQUARE_FOOT].trim());
-					int numRooms = Integer.parseInt(parts[NUM_ROOMS].trim());
-					double numBath = Double.parseDouble(parts[NUM_BATH].trim());
-					int num_floors = Integer.parseInt(parts[NUM_FLOORS].trim());
-					double backyard = Double.parseDouble(parts[BACKYARD].trim());
-					boolean rentZoning = Boolean.parseBoolean(parts[RENT_ZONING].trim());
-					boolean has_secSystem = Boolean.parseBoolean(parts[HAS_SEC_SYSTEM].trim());
-					boolean has_pool = Boolean.parseBoolean(parts[HAS_POOL].trim());
-					boolean has_fireplace = Boolean.parseBoolean(parts[HAS_FIREPLACE].trim());
-					boolean has_garage = Boolean.parseBoolean(parts[HAS_GARAGE].trim());
+		propList = new ArrayList<>();
+		Property prop;
 
-					return new Property(address, propertyType, builtIn, squareFoot, numRooms, numBath, num_floors,
-							backyard, rentZoning, has_secSystem, has_pool, has_fireplace, has_garage);
-
-				} catch (NumberFormatException e) {
-					System.out.println("Property record has a malformed integer: " + record);
-					return null;
-				}
-			} else {
-				System.out.println("Property record has the wrong number of fields: " + record);
-				return null;
-			}
-		};
-		ArrayList<Property> properties;
+		BufferedReader file = null;
 		try {
-			properties = Files.lines(Paths.get(filename))
-					.filter(record -> record.length() > 0 && record.charAt(0) != '#').map(createPropertyList)
-					.filter(property -> property != null).collect(Collectors.toCollection(ArrayList::new));
+			file = new BufferedReader(new FileReader(filename));
+			String read = null;
+			while ((read = file.readLine()) != null) {
+				String[] details = read.split("\\s+");
+				for (String part : details) {
+
+					address = details[0];
+
+					propertyType = details[1];
+
+					String builtString = details[2];
+					built = Integer.parseInt(builtString);
+
+					String squareFootString = details[3];
+					squareFoot = Integer.parseInt(squareFootString);
+
+					String numRoomsString = details[4];
+					numRooms = Integer.parseInt(numRoomsString);
+
+					String numBathString = details[5];
+					double numBath = Double.parseDouble(numBathString);
+
+					String backyardString = details[6];
+					double backyard = Double.parseDouble(backyardString);
+
+					String rentZoningString = details[7];
+					if (rentZoningString.equals("yes")) {
+						rentZoning = true;
+					}
+
+					String has_secSystemSring = details[8];
+					if (has_secSystemSring.equals("yes")) {
+						has_secSystem = true;
+					}
+
+					String has_poolString = details[9];
+					if (has_poolString.equals("yes")) {
+						has_pool = true;
+					}
+
+					String has_fireplaceString = details[10];
+					if (has_fireplaceString.equals("yes")) {
+						has_fireplace = true;
+					}
+
+					String has_garageString = details[11];
+					if (has_garageString.equals("yes")) {
+						has_secSystem = true;
+
+					}
+
+					prop = new Property(address, propertyType, built, squareFoot, numRooms, numBath, backyard,
+							rentZoning, has_secSystem, has_pool, has_fireplace, has_garage);
+					propList.add(prop);
+				}
+			}
 
 		} catch (IOException e) {
-			System.out.println("Unable to open " + filename);
-			properties = new ArrayList<>();
+			System.out.println("There was a problem: " + e);
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+				file.close();
+
+			} catch (Exception e) {
+			}
 		}
-		return properties;
+	}
+
+	/**
+	 * Returns an ArrayList of Properties from previous method
+	 */
+	public ArrayList<Property> returnProperties() {
+		return propList;
 	}
 }
