edd289b7132b4f17987c17ca6342834d2f8d660e
denisha32 <43677295+denisha32@users.noreply.github.com>
2018-11-29 23:29:01 -0500

    Update Realtor.java

--- Realtor.java
+++ Realtor.java
@@ -1,25 +1,29 @@
-package Agency;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 /**
- * The Realtor is in charge of presenting the options that match the buyers'
- * criteria, and saves any sold properties in archivedOfPropertiesSold.
+ * The Realtor class findings properties in the database that match the user's
+ * search criteria and removes from the database the property bought.
+ * 
+ * @version 2018.11.29
+ * @author D.Pimentel
  */
 public class Realtor {
-
-	private Property propertySpecs; // Specifications of the seller's property
+	
 	private ArrayList<Property> similarProperties; // Properties matching the specifications
 	private ArrayList<Property> listing; // All the properties on sale
-	private File f1 = new File("ArchiveOfPropertiesSold.txt");
+	private File f1 = new File("SoldProperties.txt");
+	private File f2 = new File("PropertiesForSale.txt");
 
 	/**
 	 * constructor to receive the specifications of the property that user wants to buy.
 	 */
-	public Realtor() {//Property propertySpecs) {
-		//this.propertySpecs = propertySpecs;
+	public Realtor() {
 		similarProperties = new ArrayList<>();
 		listing = new ArrayList<>();
 	}
@@ -32,7 +36,7 @@ public class Realtor {
 	public ArrayList<Property> getListing() throws IOException{
 
 		FileHandler propList = new FileHandler();
-		propList.retrievePropertyObject(new File ("Test.txt"));
+		propList.retrievePropertyObject(f2);
 		listing = propList.returnProperties();
 
 		return listing;
@@ -40,31 +44,24 @@ public class Realtor {
 
 	/**
 	 * Compare properties for sale in HouseListing to the buyer's specifications
+	 * @throws IOException 
 	 */
-	public void compareProperties(Property specs) {
-		/**
-		 * Sorting of files could be made from this class
-		 * 
-		 * 'if' statements to sort and save similar properties from the HouseListing
-		 * that match search criteria in propertySpecs and save in collection similarProperties
-		 *
-		 * Maybe display: No properties found matching your criteria or exception could
-		 * be added
-		 */
-		for (Property p : listing) {
-
-			buyerGUI b = new buyerGUI();
-			propertySpecs = b.getUserPropertySpecs();
-			if (propertySpecs.getPropertyType().equals(p.getPropertyType())
-					&& propertySpecs.getBuiltIn() == p.getBuiltIn()
-					&& propertySpecs.getSquareFoot() == p.getSquareFoot()
-					&& propertySpecs.getNumRooms() == p.getNumRooms()
-					&& propertySpecs.getNumBath() == p.getNumBath()
-					&& propertySpecs.getBackyard() <= p.getBackyard() 
-					&& propertySpecs.isRentZoning() == p.isRentZoning()
-					&& propertySpecs.isHas_secSystem() == p.isHas_secSystem()
-					&& propertySpecs.isHas_pool() == p.isHas_pool()
-					&& propertySpecs.isHas_fireplace() == p.isHas_fireplace())
+	public void compareProperties(Property specs) throws IOException {
+		
+		getListing(); // Create arrayList to compare
+		
+		
+		for (Property p : listing) {	
+			if (specs.getPropertyType().equals(p.getPropertyType())
+					&& specs.getBuiltIn() <= p.getBuiltIn()
+					&& specs.getSquareFoot() >= p.getSquareFoot()
+					&& specs.getNumRooms() == p.getNumRooms()
+					&& specs.getNumBath() == p.getNumBath()
+					&& specs.getBackyard() >= p.getBackyard() 
+					&& specs.isRentZoning() == p.isRentZoning()
+					&& specs.isHas_secSystem() == p.isHas_secSystem()
+					&& specs.isHas_pool() == p.isHas_pool()
+					&& specs.isHas_fireplace() == p.isHas_fireplace())
 
 				similarProperties.add(p); // Add matching property to an ArrayList
 		}
@@ -73,7 +70,7 @@ public class Realtor {
 	/**
 	 * Return the properties that match the search
 	 */
-	public ArrayList<Property> propertyMatched() {
+	public ArrayList<Property> getSimilarProperties() {
 		return similarProperties;
 	}
 
@@ -83,10 +80,6 @@ public class Realtor {
 	 */
 	public void saveSoldProperty(Property prop) throws IOException {
 
-		/**
-		 * Data could be stored in an Excel workbook, exceptions could be added here to
-		 * handle problems with the file creation
-		 */
 		try {
 			FileHandler saveProp = new FileHandler();
 			saveProp.writeFile(prop, f1);
@@ -94,6 +87,152 @@ public class Realtor {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
+		removeSoldProperty(prop);
+	}
+	
+	/**
+	 * Property sold that needs to be removed from the properties on sale
+	 * @param prop The property selected
+	 * @throws IOException
+	 */
+	public void removeSoldProperty(Property prop) throws IOException
+	{
+		String deleteProp = prop.propWrite();
+		String textInFile; 
+		
+		 File inputFile = new File("propList.txt");
+	     File tempFile = new File("ItWorks.txt");
 
+	        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+	        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+	        while((textInFile = reader.readLine()) != null) {
+	            if(null!=textInFile && !textInFile.equalsIgnoreCase(prop.propWrite())){
+	                writer.write(textInFile + System.getProperty("line.separator"));
+	            }
+	        }
+	        writer.close(); 
+	        reader.close(); 
+	        boolean successful = tempFile.renameTo(inputFile);
+	        System.out.println(successful);
+	}
+}
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * The Realtor class findings properties in the database that match the user's
+ * search criteria and removes from the database the property bought.
+ * 
+ * @version 2018.11.29
+ * @author D.Pimentel
+ */
+public class Realtor {
+	
+	private ArrayList<Property> similarProperties; // Properties matching the specifications
+	private ArrayList<Property> listing; // All the properties on sale
+	private File f1 = new File("SoldProperties.txt");
+	private File f2 = new File("PropertiesForSale.txt");
+
+	/**
+	 * constructor to receive the specifications of the property that user wants to buy.
+	 */
+	public Realtor() {
+		similarProperties = new ArrayList<>();
+		listing = new ArrayList<>();
+	}
+
+	/**
+	 * Retrieves properties saved in text file and puts them in an ArrayList to be sorted
+	 * later in compareProperties() 
+	 * @throws IOException 
+	 */
+	public ArrayList<Property> getListing() throws IOException{
+
+		FileHandler propList = new FileHandler();
+		propList.retrievePropertyObject(f2);
+		listing = propList.returnProperties();
+
+		return listing;
+	}
+
+	/**
+	 * Compare properties for sale in HouseListing to the buyer's specifications
+	 * @throws IOException 
+	 */
+	public void compareProperties(Property specs) throws IOException {
+		
+		getListing(); // Create arrayList to compare
+		
+		
+		for (Property p : listing) {	
+			if (specs.getPropertyType().equals(p.getPropertyType())
+					&& specs.getBuiltIn() <= p.getBuiltIn()
+					&& specs.getSquareFoot() >= p.getSquareFoot()
+					&& specs.getNumRooms() == p.getNumRooms()
+					&& specs.getNumBath() == p.getNumBath()
+					&& specs.getBackyard() >= p.getBackyard() 
+					&& specs.isRentZoning() == p.isRentZoning()
+					&& specs.isHas_secSystem() == p.isHas_secSystem()
+					&& specs.isHas_pool() == p.isHas_pool()
+					&& specs.isHas_fireplace() == p.isHas_fireplace())
+
+				similarProperties.add(p); // Add matching property to an ArrayList
+		}
+	}
+
+	/**
+	 * Return the properties that match the search
+	 */
+	public ArrayList<Property> getSimilarProperties() {
+		return similarProperties;
+	}
+
+
+	/**
+	 * Save sold property in ArchivedProperties
+	 */
+	public void saveSoldProperty(Property prop) throws IOException {
+
+		try {
+			FileHandler saveProp = new FileHandler();
+			saveProp.writeFile(prop, f1);
+
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		removeSoldProperty(prop);
+	}
+	
+	/**
+	 * Property sold that needs to be removed from the properties on sale
+	 * @param prop The property selected
+	 * @throws IOException
+	 */
+	public void removeSoldProperty(Property prop) throws IOException
+	{
+		String deleteProp = prop.propWrite();
+		String textInFile; 
+		
+		 File inputFile = new File("propList.txt");
+	     File tempFile = new File("ItWorks.txt");
+
+	        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+	        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+	        while((textInFile = reader.readLine()) != null) {
+	            if(null!=textInFile && !textInFile.equalsIgnoreCase(prop.propWrite())){
+	                writer.write(textInFile + System.getProperty("line.separator"));
+	            }
+	        }
+	        writer.close(); 
+	        reader.close(); 
+	        boolean successful = tempFile.renameTo(inputFile);
+	        System.out.println(successful);
 	}
 }
