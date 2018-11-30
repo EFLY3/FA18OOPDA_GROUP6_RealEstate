050066e32f0603135ef963dc9188d0c9924fa2ab
denisha32 <43677295+denisha32@users.noreply.github.com>
2018-11-29 23:31:36 -0500

    Update Residential.java

--- Residential.java
+++ Residential.java
@@ -1,27 +1,29 @@
-
+/**
+ *	Residential
+ *	subclass of Property
+ *	contains the classification Residential designator
+ *
+ *	@version 2018.11.20
+ *	@author R.Erskine 
+ */
 public class Residential extends Property
 {
 	final private String CLASSIFICATION = "Residential";
 	
-	public Residential()
-	{
+	public Residential(){
 		super();
 	}
 	
-	public String prop_write()
-	{
+	public String prop_write(){
 			return ""+ CLASSIFICATION + " " + address + " " + propertyType + " " + builtIn + " " + squareFoot + " "
 					+ numRooms + " " + numBath + " " + backyard + " " + rentZoning + " " + has_secSystem + " " 
-					+ has_pool + " " + has_fireplace + " " + value + " ";
-		
+					+ has_pool + " " + has_fireplace + " " + value + " ";	
 	}
 	
-	public void printProp()
-	{
+	public void printProp(){
 		System.out.println(
 				""+ CLASSIFICATION + "\n" + address + "\n" + propertyType + "\n" + builtIn + "\n" + squareFoot + "\n"
 						+ numRooms + "\n" + numBath + "\n" + backyard + "\n" + rentZoning + "\n" + has_secSystem + "\n" 
 						+ has_pool + "\n" + has_fireplace + "\n" + value + "\n");
 	}
-
 }
