33588c12eab0779c371544834d47050c0668eabd
denisha32 <43677295+denisha32@users.noreply.github.com>
2018-11-29 23:24:22 -0500

    Update Commercial.java

--- Commercial.java
+++ Commercial.java
@@ -1,27 +1,30 @@
-
+package agency;
+/**
+ *	Commercial
+ *	subclass of Property
+ *	contains the classification Commercial designator
+ 
+ * 	@version 2018.11.20
+ *	@author R.Erskine 
+ */
 public class Commercial extends Property
 {
 	final private String CLASSIFICATION = "Commercial";
 	
-	public Commercial()
-	{
+	public Commercial(){
 		super();
 	}
 	
-	public String prop_write()
-	{
+	public String prop_write(){
 			return ""+ CLASSIFICATION + " " + address + " " + propertyType + " " + builtIn + " " + squareFoot + " "
 					+ numRooms + " " + numBath + " " + backyard + " " + rentZoning + " " + has_secSystem + " " 
-					+ has_pool + " " + has_fireplace + " " + value + " ";
-		
+					+ has_pool + " " + has_fireplace + " " + value + " ";	
 	}
 	
-	public void printProp()
-	{
+	public void printProp(){
 		System.out.println(
 				""+ CLASSIFICATION + "\n" + address + "\n" + propertyType + "\n" + builtIn + "\n" + squareFoot + "\n"
 						+ numRooms + "\n" + numBath + "\n" + backyard + "\n" + rentZoning + "\n" + has_secSystem + "\n" 
 						+ has_pool + "\n" + has_fireplace + "\n" + value + "\n");
 	}
-
 }
