21905265459f300c5efc946cf969b84a69d09ff7
denisha32 <43677295+denisha32@users.noreply.github.com>
2018-11-29 23:23:01 -0500

    Update Appraiser.java

--- Appraiser.java
+++ Appraiser.java
@@ -1,19 +1,14 @@
-
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 
-   /** The Appraiser class will assign a value to a home based on it's internal analyzer.
-	 * 
-	 *  The analysis considers specific components, such as: 
-		 * -square footage 
-		 * -acreage 
-		 * -other features such as security systems, pools and fireplaces.
-	 *
-	 * - edited by R.Erskine 11/15/18
-	 */
+/** The Appraiser class will assign a value to a home based on it's internal analyzer.
+ *  The analysis considers specific property features.
+ *  
+ * @version 2018.11.29
+ * @author D.Pimentel
+ * edited by R.Erskine 11/15/18
+ * edited by R.Erskine 11/28/18
+ */
 public class Appraiser { 
 	
 	//the built in prices the Appraiser uses to calculate the cost of a house
@@ -27,64 +22,55 @@ public class Appraiser {
 	//the house costs
 	private double propertyValue;
 	
+	private File f1 = new File("PropertiesForSale.txt");
+	
 	/** 
-	 * default constructor
-	 * 		This is the ACME appraiser, with no custom pricing
+	 * Default constructor: this is the ACME appraiser, with no custom pricing
 	 */
 	public Appraiser() 
 	{
-
 		pricePerSQFT = 150.00;
 		pricePerAcre = 10000;
 		priceSecSys = 15000;
 		pricePool = 20000;
 		priceFire = 7000;
-		priceGarage = 5000;
-		
+		priceGarage = 5000;	
 		propertyValue = 0.0;
 	}
-
 
 	/** 
 	 * Calculate price of seller's property
 	 */ 
 	public void calculatePropertyValue(int sqft, double acre, boolean sec, boolean pool, 
-										boolean fire, boolean gara) 
-	{	
+										boolean fire, boolean gara) {	
 		double total = 0.0;
 		
 		total += sqft * pricePerSQFT;
 		total += acre * pricePerAcre;
 		
-		if(sec == true)
-		{
+		if(sec == true){
 			total += priceSecSys;
 		}
 		
-		if(pool == true)
-		{
+		if(pool == true){
 			total += pricePool;
 		}
 		
-		if(fire == true)
-		{
+		if(fire == true){
 			total += priceFire;
 		}
 		
-		if(gara == true)
-		{
+		if(gara == true){
 			total += priceGarage;
 		}
-	
+		
 		propertyValue = total;
-	
 	}
 
 	/** 
 	 * Return calculated price of property 
 	 */ 
-	public double getPropertyValue() 
-	{
+	public double getPropertyValue() {
 		return propertyValue;
 	}
 		
